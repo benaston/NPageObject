c1f176dd8e48e00c8ae0ed21306304e8f8cad1cf
Ben Aston <ben.aston@justgiving.com>
2011-10-08 00:13:52 +0100

    Removal of selenium subsystem.

--- src/NPageObject.Selenium/RepeatedlyInvocableDelegate.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace NPageObject.Selenium
-{
-    public delegate ShouldRepeatDelegateInvocation RepeatedlyInvocableDelegate<in TDelegateDto, TOutputValue>(
-        TDelegateDto dto, out TOutputValue returnValue);
-}
--- src/NPageObject/Properties/AssemblyInfo.cs
+++ src/NPageObject/Properties/AssemblyInfo.cs
@@ -1,39 +1,14 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("NPageObject")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("e8656772-e4cd-40e1-854a-cae5059dd4c9")]
+[assembly: AssemblyDescription("e8656772-e4cd-40e1-854a-cae5059dd4c9")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("NPageObject")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Ben Aston")]
+[assembly: AssemblyProduct("e8656772-e4cd-40e1-854a-cae5059dd4c9")]
+[assembly: AssemblyCopyright("Copyright © Ben Aston 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
 [assembly: Guid("3b139ff1-e579-4c7b-95ee-2c266b4456bf")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
